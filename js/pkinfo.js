6d30864adc5b54fb05d5b14e777abe63ea260b99
aalizzwall <p331090144@hotmail.com>
2015-07-02 17:41:44 +0800

    Update pkinfo.js

--- js/pkinfo.js
+++ js/pkinfo.js
@@ -1,11 +1,17 @@
   angular.module('pkApp',['ui.bootstrap'])
-    .controller('pkctl', ['$scope', '$http',
-        function($scope, $http) {
-          $scope.parks = [];
-          $http.get('http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=fe6767ec-9732-4bc9-b051-0990087c708d')
-          .success(function(data) {
+    .controller('pkctl', ['$scope', '$http', function($scope, $http) {
+          $http.get('http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=fe6767ec-9732-4bc9-b051-0990087c708d',
+          {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+              'User-Agent': 'APPLICATION NAME',
+              'Authorization': 'Basic INSERT BASE 64 ENCODED LOGIN CREDENTIALS HERE',
+              'Access-Control-Allow-Origin': 'http://run.plnkr.co'
+            }  
+          }).success(function(data) {
             $scope.parks = data[result][results];
           });  
         }
-    ])
+    ]);
 
