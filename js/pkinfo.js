677d224e94e1df1c233adb23323845c22262fc13
aalizzwall <p331090144@hotmail.com>
2015-07-02 14:10:47 +0800

    Update pkinfo.js

--- js/pkinfo.js
+++ js/pkinfo.js
@@ -4,13 +4,20 @@
     .controller('pkctl', ['$scope', '$http',
         function($scope, $http) {
           $scope.parks = [];
+          $http.get('http://data.taipei.gov.tw/opendata/apply/query/QzhBMEJFOTctMEEzRC00M0Q2LThDNDktNDVCNDc3NDNDRDBC?$format=json', {
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'APPLICATION NAME',
+      'Authorization': 'Basic INSERT BASE 64 ENCODED LOGIN CREDENTIALS HERE'
+    }
+  }).success(function(data) {
+    $scope.parks = data;
+  }).error(function(data, status, headers, config) {
+    console.log(config);
+  });  
 
-          
-          $http.get('data.json').success (function(data){
-				    $scope.parks = data;
-		      });
-
-        }
     ])
+
 
 })();
