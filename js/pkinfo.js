6a16c60a381108ea496d9a2eeffb6a08fc7c3e71
aalizzwall <p331090144@hotmail.com>
2015-07-02 14:15:10 +0800

    Update pkinfo.js

--- js/pkinfo.js
+++ js/pkinfo.js
@@ -1,23 +1,22 @@
-(function() {
   angular.module('pkApp',['ui.bootstrap'])
-
     .controller('pkctl', ['$scope', '$http',
         function($scope, $http) {
           $scope.parks = [];
           $http.get('http://data.taipei.gov.tw/opendata/apply/query/QzhBMEJFOTctMEEzRC00M0Q2LThDNDktNDVCNDc3NDNDRDBC?$format=json', {
-    withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'APPLICATION NAME',
-      'Authorization': 'Basic INSERT BASE 64 ENCODED LOGIN CREDENTIALS HERE'
-    }
-  }).success(function(data) {
-    $scope.parks = data;
-  }).error(function(data, status, headers, config) {
-    console.log(config);
-  });  
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+              'User-Agent': 'APPLICATION NAME',
+              'Authorization': 'Basic INSERT BASE 64 ENCODED LOGIN CREDENTIALS HERE'
+            }  
+            
+          }).success(function(data) {
+            $scope.parks = data;
+            
+          }).error(function(data, status, headers, config) {
+            console.log(config);
+            
+          });  
 
-    ])
+    ]);
 
-
-})();
