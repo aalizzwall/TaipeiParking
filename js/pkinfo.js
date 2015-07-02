fa0f83f6bcf39dd7fcbb3e6796e0757d7eefc22d
aalizzwall <p331090144@hotmail.com>
2015-07-02 17:56:51 +0800

    Update pkinfo.js

--- js/pkinfo.js
+++ js/pkinfo.js
@@ -1,16 +1,7 @@
   angular.module('pkApp',['ui.bootstrap'])
     .controller('pkctl', ['$scope', '$http', function($scope, $http) {
-          $http.get('http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=fe6767ec-9732-4bc9-b051-0990087c708d',
-          {
-            withCredentials: true,
-            headers: {
-              'Content-Type': 'application/json',
-              'User-Agent': 'APPLICATION NAME',
-              'Authorization': 'Basic INSERT BASE 64 ENCODED LOGIN CREDENTIALS HERE',
-              'Access-Control-Allow-Origin': '*'
-            }  
-          }).success(function(data) {
-            $scope.parks = data[result][results];
+          $http.get('http://opendata.hccg.gov.tw/dataset/8c25cb86-5ac9-4eb4-9337-7a4712aaefc8/resource/7e147049-11a5-4adc-ae15-d2f2536fe567/download/20150311113638455.json'}).success(function(data) {
+            $scope.parks = data;
           });  
         }
     ]);
