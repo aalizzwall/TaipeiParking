a59cffe9fb9ed3a096224c995f9bc74db2e8b834
aalizzwall <p331090144@hotmail.com>
2015-07-02 17:16:01 +0800

    change source

--- js/pkinfo.js
+++ js/pkinfo.js
@@ -5,9 +5,9 @@
         function($scope, $http) {
           $scope.parks = [];
 
-          $http.get('http://data.taipei.gov.tw/opendata/apply/query/QzhBMEJFOTctMEEzRC00M0Q2LThDNDktNDVCNDc3NDNDRDBC?$format=json')
+          $http.get('http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=fe6767ec-9732-4bc9-b051-0990087c708d')
             .success(function(result) {
-              $scope.parks = result.data;
+              $scope.parks = result[result][results];
             });
 
         }
